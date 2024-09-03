d13ff2444c0e68b7569929b672706d3effa806fc
Adam Caprez <acaprez2@unl.edu>
2024-09-03 22:23:39 +0000

    Retain .singularity libs path for topaz and deepemhancer.
    This path is needed for external GPU libs to be seen inside
    the container.

--- deepemhancer.sh
+++ deepemhancer.sh
@@ -13,7 +13,7 @@ unset CONDA_PYTHON_EXE
 unset CONDA_SHLVL
 unset PYTHONPATH
 unset LD_PRELOAD
-unset LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/.singularity.d/libs
 
 source /opt/conda/etc/profile.d/conda.sh
 conda activate /opt/deepEMhancer_env
